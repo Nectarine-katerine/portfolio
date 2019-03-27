02bcbc6fcacc47d53ed5e5822f5ad567bfc545e5
Ekaterina <extra33333@mail.ru>
2019-03-27 23:33:50 +0300

    added login, header, menu and about me sections to admin

--- src/admin/App.vue
+++ src/admin/App.vue
@@ -10,39 +10,85 @@
               .log-in.form__block
                 label(for="log-in").form__block-title Логин
                 .input
+                  svg.form__block-icon
+                    use(xlink:href="sprite.svg#avatar")
                   input(id="log-in" type="text" name="log-in" placeholder="Terminator_2000").form__block-input
             .form__row
               .password.form__block
                 label(for="password").form__block-title Пароль
                 .input
-                  input(id="password" type="password" name="password").form__block-input
+                  svg.form__block-icon
+                    use(xlink:href="sprite.svg#key")
+                  input(id="password" type="password" name="password" placeholder="***********").form__block-input
             button(type="submit").button Отправить
-    header.header
-      .header__container
-        .header__col.header__col_flex
-          .user
+    .page.page__about
+      header.header
+        .container.header__container
+          .header__col.header__col_grid
             .user__photo
               img(src="../images/content/me.jpeg" alt="Личное фото Екатерины Исмиляевой").user__pic
             .user__name Екатерина Исмиляева
-          h2.header__title Панель администрирования
-        .header__col
-          button(type="button") Выйти
+            h3.header__title Панель администрирования
+          .header__col.header__col_end
+            button(type="button").exit__btn Выйти
 
+      nav.menu
+        .container.menu__container
+          ul.menu__list
+            li.menu__item.menu__item_active
+              a.menu__link(href="#") Обо мне
+            li.menu__item
+              a.menu__link(href="#") Работы
+            li.menu__item
+              a.menu__link(href="#") Отзывы
+      main.skills
+        .container.skills__container
+          button(type="button").skills__add Добавить группу
+          ul.skills__list
+            li.skills__item
+              .skills__inner
+                form.skills__form.skills__group
+                  .skills__form-row
+                    .skills__form-col
+                      input(type="text" placeholder="Название новой группы").group__name
+                    .skills__form-col.skills__form-col_end
+                      svg.skills__form-icon
+                        use(xlink:href="sprite.svg#tick")
+                      svg.skills__form-icon.skills__form-icon_cross
+                        use(xlink:href="sprite.svg#cross")
+                  .skills__form-row.skills__form-row_three-col
+                    input(type="text" placeholder="Новый навык").skill__new
+                    input(type="text" placeholder="100%").skill__percent
+                    button(type="button").skill__add +
 
+            li.skills__item
+              .skills__inner
+            li.skills__item
+              .skills__inner
 </template>
 
 <style lang=postcss>
   * {
     margin: 0;
     padding: 0;
+    box-sizing: border-box;
+  }
+  ul, li {
+    list-style-type: none;
+    margin: 0;
+    padding: 0;
+  }
+  .main {
+    font-family: 'Open Sans';
   }
   .login {
-    background: url(../images/content/train.png) center center / cover no-repeat;
+    background: url(../images/content/admin_bg.png) center center / cover no-repeat;
     height: 100vh;
   }
+
   .login__overlay {
     height: 100vh;
-    background-color: rgba(#2d3c4e, .7);
+    background-color: rgba(#2d3c4e, .9);
     position: relative;
   }
   .login__container {
@@ -91,7 +137,6 @@
     text-align: center;
     margin-bottom: 34px;
     color: #414c63;
-    font-family: Open Sans;
     font-size: 36px;
     font-weight: 600;
   }
@@ -102,11 +147,10 @@
   .form__block-title {
     opacity: 0.3;
     color: #414c63;
-    font-family: Open Sans;
     font-size: 16px;
     font-weight: 600;
-    margin-bottom: 25px;
     display: block;
+    margin-left: 46px;
   }
 
   .form__block-input {
@@ -122,38 +166,56 @@
       font-weight: 700;
     }
   }
+  .input {
+    display: flex;
+    align-items: center;
+  }
+  .form__block-icon {
+    width: 28px;
+    height: 28px;
+    fill: rgba(#414c63, .3);
+    padding-right: 18px;
+  }
   .button {
     display: block;
     padding: 25px 99.2px;
     outline: none;
-    background-image: linear-gradient(to right, #ea7400 0%, #f29400 100%);  
+    background-image: linear-gradient(to right, #ad00ed 0%, #5500f2 100%);  
     text-transform: uppercase;
     font-size: 18px;
     font-weight: 700;
     color: #fff;
     margin-top: 20px;
     border: none;
+    border-radius: 40px 5px;
     margin: 0 auto;
+    cursor: pointer;
     &:hover {
-      background-image: linear-gradient(to right, #ff7f00 0%, #ff9d00 100%);
+      background-image: linear-gradient(to right, #ad00ed 50%, #5500f2 100%);
     }
+  }
+  .container {
+    width: 95%;
+    margin: 0 auto;
+
   }
   .header {
     background-image: linear-gradient(to right, #3e3e59 0%, #454573 100%);
   }
 
   .header__container {
-    width: 95%;
-    margin: 0 auto;
-    display: flex;
-    justify-content: space-between;
+    padding: 12px 0;
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    grid-template-rows: 1fr;
   }
-  .header__col_flex {
-    display: flex;
+  .header__col_grid {
+    display: grid;
+    grid-template-columns: 0.4fr 1fr 1fr;
+    grid-template-rows: 1fr;
+    align-items: center;
   }
-  .user {
-    display: flex;
-  }
+
   .user__photo {
   width: 45px;
   height: 45px;
@@ -166,5 +228,154 @@
    max-width: 100%;
    max-height: 100%;
    display: block;
+}
+
+.user__name {
+  color: #ffffff;
+  font-weight: 600;
+  font-size: 18px;
+}
+
+.header__title {
+  color: rgba(#fff, .5);
+  font-size: 14px;
+}
+
+.header__col {
+  align-self: center;
+}
+
+.header__col_end{
+  justify-self: end;
+  align-self: center;
+
+}
+
+.exit__btn {
+  display: block;
+  outline: none;
+  background: transparent;
+  text-decoration: underline;
+  border: none;
+  color:rgba(#fff, .7);
+  font-family: "Open Sans";
+  font-size: 14px;
+  cursor: pointer;
+}
+.menu {
+  background: #fff;
+}
+.menu__list {
+  list-style-type: none;
+  display: grid;
+  grid-template-columns: .1fr .1fr .1fr;
+  grid-template-rows: 1fr;
+
+}
+.menu__item {
+  border-bottom: 3px solid transparent;
+  text-align: center;
+
+  &_active {
+    border-color: #383bcf;
+    .menu__link {
+    color:  #383bcf;
+    }
+  }
+}
+.menu__link {
+  display: inline-block;
+  text-decoration: none;
+  color: #414c63;
+  font-family: "Open Sans";
+  font-size: 16px;
+  font-weight: 400;
+  padding: 30px 0;
+}
+.page__about {
+    background: url(../images/content/admin_bg.png) center center / cover no-repeat;
+    position: relative;
+    z-index: -2;
+    &::before {
+      content:'';
+      position: absolute;
+      top: 0;
+      left: 0;
+      right: 0;
+      bottom: 0;
+      background: rgba(#fff, .8);
+      z-index: -1;
+    }
+}
+.skills__list {
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  grid-column-gap: 30px;
+  grid-row-gap: 30px;
+}
+.skills__item {
+  box-shadow: 4px 3px 20px rgba(0, 0, 0, 0.07);
+  background-color: #ffffff; 
+  height: 250px; 
+}
+
+.skills__add {
+  color: #383bcf;
+  font-size: 16px;
+  font-weight: 600;
+  outline: none;
+  border: none;
+  background: transparent;
+  padding: 60px 200px;
+  &:before {
+    margin-right: 13px;
+    content: '+';
+    font-size: 15px;
+    color: #fff;
+    padding: 4px 8px;
+    border-radius: 100%;
+    background-image: linear-gradient(to right, #006aed 0%, #3f35cb 100%);
+  }
+}
+.skills__inner {
+  width: 95%;
+  margin: 0 auto;
+  height: 100%;
+}
+.skills__form {
+  justify-content: space-between;
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+}
+.skills__form-row {
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  grid-template-rows: 0.2fr;
+}
+.skills__form-row_three-col {
+    grid-template-columns: 1fr .5fr .2fr;
+    justify-items: end;
+    grid-gap: 30px;
+}
+.skills__form-col_end {
+  justify-self: end;
+}
+.group__name {
+  border: none;
+  background: transparent;
+  display: block;
+  padding: 10px 0;
+  border-bottom: 1px solid #000;
+}
+.skills__form-icon {
+  width: 15px;
+  height: 12px;
+  margin-right: 20px;
+  fill: #00d70a;
+}
+.skills__form-icon_cross {
+  margin-right: 0;
+  fill: #bf2929;
 }
 </style>
--- src/scripts/parallax.js
+++ src/scripts/parallax.js
@@ -4,14 +4,32 @@ const layers = parallax.children;
 function moveLayersDependsOnScroll(wScroll) {
 
   Array.from(layers).forEach(layer => {
-    const divider = layer.dataset.speed;
-    const strafe = wScroll*divider;
+    var divider = layer.dataset.speed;
+    var strafe = wScroll*divider;
 
     layer.style.transform = `translateY(-${strafe}%)`;
 });
 }
 
-  window.addEventListener('scroll', e => {
-    const wScroll = window.pageYOffset;
-    moveLayersDependsOnScroll(wScroll);
-  });
+  // window.addEventListener('scroll', e => {
+  //   const wScroll = window.pageYOffset;
+  //   moveLayersDependsOnScroll(wScroll);
+  // });
+
+  // const feedbackParallax=document.querySelector('.feedback__parallax');
+  // const pictures = feedbackParallax.children;
+
+  // function moveLayersOnScroll(wScroll) {
+
+  //   Array.from(pictures).forEach(picture => {
+  //     divider = picture.dataset.speed;
+  //     strafe = wScroll*divider;
+  
+  //     picture.style.transform = `translateY(-${strafe}%)`;
+  // });
+  // }
+
+  // window.addEventListener('scroll', e => {
+  //   wScroll = window.pageYOffset;
+  //   moveLayersOnScroll(wScroll);
+  // });
