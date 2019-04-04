eceed5981472ed6b668866b675661050596cf8ce
Ekaterina <extra33333@mail.ru>
2019-04-04 14:46:41 +0300

    added input_error class

--- src/scripts/form.js
+++ src/scripts/form.js
@@ -3,44 +3,13 @@ const formButton=document.querySelector('#formButton');
 
 formButton.addEventListener('click', function(event){
   event.preventDefault();
-  if (validateForm(myForm)) {
-    const name=myForm.elements.name.value;
-    const email=myForm.elements.email.value;
-    const comment=myForm.elements.comment.value;
-    var formData=new FormData();
-    formData.append('name', name);
-    formData.append('email', email);
-    formData.append('comment', comment);
-    const xhr=new XMLHttpRequest();
-    xhr.responseType='json';
-    xhr.open('POST', 'https://webdev-api.loftschool.com');
-    xhr.send(formData);
-    xhr.addEventListener('load', e => {
-      if (xhr.response.status){
-        console.log('ok');
-      } else {
-        console.log('no ok');
-      }
-    });
+  function showError(input) {
+    var input = document.querySelectorAll ('.input')
+
+    for (i=0; i<input.length; i++) {
+      input[i].classList.add='input_error'
+    }
   }
-});
-//             modal.setContent('Сообщение отправлено');
-//             modal.open();
-//             setTimeout(e=>{
-//                 modal.close();
-//             }, 3000);
-            
-//     } else if ((xhr.response.status == 0)) {
-//         modal.setContent('Отправить письмо не удалось, повторите запрос позже');
-//         modal.open();
-//         setTimeout(e=>{
-//           modal.close();
-//       }, 3000);
-//     }
-//     });
-//   }
-// });
-  
   function validateForm(myForm) {
 
     let valid = true;
@@ -58,148 +27,13 @@ formButton.addEventListener('click', function(event){
   };
 
   function validateField(field) {
-    if (!field.checkValidity()){
-      field.nextElementSibling.textContent = field.validationMessage;
-      return false;
-    }
-    else {
-        field.nextElementSibling.textContent = '';
+
+      if (!field.checkValidity()){
+        showError();
+        return false;
+      } else {
+        input.classList.remove='input_error'
         return true;
-    }
-  };
-
-
-//     const name=myForm.elements.name.value;
-//     const email=myForm.elements.email.value;
-//     const comment=myForm.elements.comment.value;
-//     var formData=new FormData();
-//     formData.append('name', name);
-//     formData.append('email', email);
-//     formData.append('comment', comment);
-//     const xhr=new XMLHttpRequest();
-//     xhr.responseType='json';
-//     xhr.open('POST', 'https://webdev-api.loftschool.com/sendmail');
-//     xhr.send(formData);
-//     xhr.addEventListener('load', e => {
-//       if (xhr.response.status){
-//             modal.setContent('Сообщение отправлено');
-//             modal.open();
-//             setTimeout(e=>{
-//                 modal.close();
-//             }, 3000);
-            
-//     } else if ((xhr.response.status == 0)) {
-//         modal.setContent('Отправить письмо не удалось, повторите запрос позже');
-//         modal.open();
-//         setTimeout(e=>{
-//           modal.close();
-//       }, 3000);
-//     }
-//     });
-//   }
-// });
-
-
-
-
-
-// const button=document.querySelector('#formButton');
-// const template = document.querySelector("#modal-template").innerHTML;
-// var wrapper = document.querySelector(".wrapper");
-// const modal = createModal();
-
-// function createModal() {
-//   var container = document.createElement('div');
-//   container.className = 'popup';
-//   container.innerHTML = template;
-
-//   const contentBlock = container.querySelector('.popup__content');
-
-//   var closeBtn=container.querySelector('.popup__close');
-
-//   closeBtn.addEventListener('click', e => {
-//     event.preventDefault();
-//     wrapper.removeChild(container);
-//   })
-
-//   var overlay = container.querySelector('.overlay');
-//   overlay.addEventListener('click', e => {
-//     if (e.target === overlay) {
-//       closeBtn.click();
-//     }
-//   })
-//   return {
-//     open(){
-//       wrapper.appendChild(container);
-//     },
-//     close(){
-//       closeBtn.click();
-//     },
-//     setContent(content){
-//       contentBlock.innerHTML=content;
-//     }
-//   };
-// }
-
-
-// const myForm=document.querySelector('#myForm');
-// const formButton=document.querySelector('#formButton');
-
-// formButton.addEventListener('click', function(event){
-//   event.preventDefault();
-//   if (validateForm(myForm)) {
-//     const name=myForm.elements.name.value;
-//     const email=myForm.elements.email.value;
-//     const comment=myForm.elements.comment.value;
-//     var formData=new FormData();
-//     formData.append('name', name);
-//     formData.append('email', email);
-//     formData.append('comment', comment);
-//     const xhr=new XMLHttpRequest();
-//     xhr.responseType='json';
-//     xhr.open('POST', 'https://webdev-api.loftschool.com/sendmail');
-//     xhr.send(formData);
-//     xhr.addEventListener('load', e => {
-//       if (xhr.response.status){
-//             modal.setContent('Сообщение отправлено');
-//             modal.open();
-//             setTimeout(e=>{
-//                 modal.close();
-//             }, 3000);
-            
-//     } else if ((xhr.response.status == 0)) {
-//         modal.setContent('Отправить письмо не удалось, повторите запрос позже');
-//         modal.open();
-//         setTimeout(e=>{
-//           modal.close();
-//       }, 3000);
-//     }
-//     });
-//   }
-// });
-
-// function validateForm(myForm) {
-//   let valid = true;
-
-//   if(!validateField(myForm.elements.name)) {
-//     valid=false;
-//   }
-//   if(!validateField(myForm.elements.email)) {
-//     valid=false;
-//   }
-//   if(!validateField(myForm.elements.comment)) {
-//     valid=false;
-//   }
-//   return valid;
-// };
-
-// function validateField(field) {
-//   if (!field.checkValidity()){
-//     field.nextElementSibling.textContent = field.validationMessage;
-//     return false;
-// }
-// else {
-//     field.nextElementSibling.textContent = '';
-//     return true;
-// }
-// }
+      }
+  }
+});
